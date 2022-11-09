e1c5eb6623816422534bfe038f9d72c96ec99b8c
Your Name <you@example.com>
2022-11-09 18:44:54 -0500

    advert2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/Head";
+import Head from "next/head";
 import Header from '../components/Header';
 import Banner from "../components/Banner";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
