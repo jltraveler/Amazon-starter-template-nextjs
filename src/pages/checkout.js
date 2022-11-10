0c03d20f7847b8aa2933ebaf1a2da8f48b99bbc4
Your Name <you@example.com>
2022-11-10 16:13:57 -0500

    add items to basket

---
+++ src/pages/checkout.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Header from '../components/Header'
+import Image from 'next/image'
+
+function checkout() {
+  return (
+      <div className='bg-gray-100'>
+          <Header />
+          <main className='lg:flex max-w-screen-2xl mx-auto'>
+              {/* left */}
+              <div className='flex-grow m-5 shadow-sm'>
+                  <Image
+                      src='https://links.papareact.com/ikj'
+                      width={1020}
+                      height={250}
+                      objectfit='contain'
+                  />
+                  <div className='flex flex-col p-5 space-y-10 bg-white'>
+                      <h1 className='text-3xl border-b pb-4'>Shopping Cart</h1>
+                  </div>
+
+              </div>
+
+              {/* right */}
+
+          </main>
+    </div>
+  )
+}
+
+export default checkout
