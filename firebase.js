844b2ffbf9f4c6cadb9d1908a1b812ed8aa6e146
Your Name <you@example.com>
2022-11-09 21:18:24 -0500

    nextAuthbeforeitbreaks

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA6mlBm4Yvr5RAqwvepCBs1Twu-NbIgXkw",
+    authDomain: "amzn-9b8e3.firebaseapp.com",
+    projectId: "amzn-9b8e3",
+    storageBucket: "amzn-9b8e3.appspot.com",
+    messagingSenderId: "754719722800",
+    appId: "1:754719722800:web:fb4c34f9cc44275a9e0cf5"
+};
